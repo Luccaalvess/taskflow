14f30b6353068288aa7fd4168bd7abe16c694e68
luccaalvess <lucca-alves@hotmail.com>
2026-07-31 15:11:49 -0300

    feat: adiciona rota inicial da API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,20 @@
 import express from "express";
 import cors from "cors";
 
+// Criamos a aplicação.
 const app = express();
 
+// Middlewares
+// Essa configuração faz com que o Express consiga interpretar requisições com corpo em JSON.
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+
+
+    res.status(200).json({ message: "TaskFlow API" })
+
+})
+
 
 export { app };
